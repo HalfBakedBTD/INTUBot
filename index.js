16c8ee5d3195d0e6983635e0e3f0f77c9595c1b1
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-10 14:04:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
-const DBL = require("dblapi.js");
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ2Mjc4MzE2MDEyODc2NTk1MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTM2MTEyMzIzfQ.vUIOdUSuw3hEHsqjNiOyifQALYELr4plSv7oHEhvS1w', bot);
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 
@@ -25,25 +23,17 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.users.filter(u => u.id !== 'ads').forEach(user => {
-    if (user.id !== '275831434772742144' {
-      user.send(`AdBot will be shutting down in ~30 days.\nShout To: https://discord.gg/rJS2P6`)
-    }
-  });
-  
-  
+  console.log(`${bot.user.username} is online!`);  
 
-  bot.user.setGame(`^help | ^invite`);
+  bot.user.setGame(`.help`);
 });
 
 bot.on('guildCreate', guild => {
-  bot.user.setGame(`^help | ^invite`);
+  bot.user.setGame(`.help`);
 });
 
 bot.on("message", async message => {
   if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
 
   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
   if(!prefixes[message.guild.id]){
