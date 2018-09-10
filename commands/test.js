b57a45a5f098c2dec4cbfce86d445c6b5bd01f5d
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-10 14:19:32 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,15 @@ const api = "https://wallet.intucoin.com/apidocs/swagger.json";
 const snekfetch = require("snekfetch");
 
 module.exports.run = async (bot, message, args) => {
-    snekfetch.get(api).then(r => console.log(r));
+    snekfetch.get(api).then(r => {
+        let body = r.body;
+        let id = args[0];
+        if(!id) return;
+        if(isNan(id)) return;
+        
+        let entry = body.find(post => post.id === id);
+        console.log(entry);
+    });
 }
 
 module.exports.help = {
